64ad227b4a1a5886a095d66a6b9a935cd222391f
Rhaegal <dapsjj@qq.com>
2019-01-25 21:32:25 +0800

    Update gambling.py

--- gambling.py
+++ gambling.py
@@ -1,33 +1,34 @@
 import numpy as np
-import math
 
 exitFlag=False
 money = int(input('请输入你的赌资:'))
-if money>1:
-    print('赌资:'+str(money))
-    while money > 1:
-        for i in range(10000):
-            # yazhu = int(input('请输入押注金额:'))
-            print('当前投注金额:'+str(math.pow(2,i)))
-            if money<2:
-                print('对不起。赌资太少，欢迎下次再来。')
-                exitFlag = True
-                break
-            else:
-                randNum = np.random.randint(2)#随机数0和1,0代表赌博输了,1代表赌博赢了
-                if randNum==0:
-                    money = money - math.pow(2, i)
-                else:
-                    money = money + 2*math.pow(2, i)
-                print('当前剩余赌资:' + str(money))
-                if math.pow(2, i+1)>money:
-                    print('对不起。赌资剩余:'+str(money)+'小于下轮投注金额:'+str(math.pow(2, i+1))+',赌博结束。')
-                    exitFlag = True
-                    break
-        if exitFlag:
-            print('赌博了'+str(i+1)+'把.')
+# money = 10000
+yazhu = 100
+count = 0
+print('赌资:'+str(money))
+if money > 100:
+    for i in range(10000):
+        count += 1
+        print('当前投注金额:'+str(yazhu))
+        randNum = np.random.randint(2)#随机数0和1,0代表赌博输了,1代表赌博赢了
+        if randNum==0:
+            money = money - yazhu
+            yazhu *= 2#赌输了则下轮投注翻倍
+        else:
+            money = money + 2*yazhu
+        print('当前剩余赌资:' + str(money))
+
+        if money<yazhu: #赌资不够下一轮押注，则押注金额重置为100
+            print('赌资剩余:' + str(money) + ',小于'+str(yazhu)+',押注重置成100。')
+            yazhu = 100
+
+        if money < 100:
+            print('对不起。赌资剩余:'+str(money)+',小于100,赌博结束。')
+            exitFlag = True
             break
 
+
+    print('赌博了'+str(count)+'把.')
 
 else:
     print('对不起。赌资太少，欢迎下次再来。')
